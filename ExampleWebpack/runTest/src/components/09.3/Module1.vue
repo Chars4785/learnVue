19a734ed63029b6e2f0a745d1c1905cd551a5db8
Unknown <9105lgm@naver.com>
2019-03-11 15:29:53 +0900

    ModuleCss

---
+++ ExampleWebpack/runTest/src/components/09.3/Module1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button :class="$style.hand"> 단일 </button>
+        <button :class="[$style.hand, $style.border]"> 여러개  </button>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log(this.$style);
+    }
+}
+</script>
+
+<style module>
+.hand{ cursor:pointer; background-color: purple;  color:yellow;}
+</style>
+
+
