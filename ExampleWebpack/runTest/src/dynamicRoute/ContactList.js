f498ba8c1ae4d2f88bacf3ecce228fe476e76110
Unknown <9105lgm@naver.com>
2019-03-13 15:40:29 +0900

    ContactListRoute

---
+++ ExampleWebpack/runTest/src/dynamicRoute/ContactList.js
@@ -0,0 +1,10 @@
+var contactlist = {
+    contacts: [
+        { no: 101, name: '이종민', tel: '010' },
+        { no: 102, name: '이종민2', tel: '020' },
+        { no: 103, name: '이종민3', tel: '030' },
+        { no: 104, name: '이종민4', tel: '040' },
+    ]
+}
+
+export default contactlist;
